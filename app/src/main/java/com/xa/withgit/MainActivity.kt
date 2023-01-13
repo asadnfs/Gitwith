8ecd6297a8eada65f70ae52206afdd57f65569ba
asadbek <asadnfs>
2023-01-13 22:03:04 +0500

    a

--- app/src/main/java/com/xa/withgit/MainActivity.kt
+++ app/src/main/java/com/xa/withgit/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.xa.withgit
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.push_btn)
+
+        btn.setText("cake")
 
     }
 
